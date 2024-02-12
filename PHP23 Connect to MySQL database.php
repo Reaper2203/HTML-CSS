8dbb6abeabd762f8ee6f561e9fa60c0aba4a64ad
Reaper2203 <134066384+Reaper2203@users.noreply.github.com>
2024-02-12 08:14:00 +0800

    Update PHP23 Connect to MySQL database.php

--- PHP23 Connect to MySQL database.php
+++ PHP23 Connect to MySQL database.php
@@ -1,5 +1,14 @@
 <?php
     // 1. MySQLi Extension
     // 2. PDO (PHP Data Objects)
-    
+
+    /* steps... 
+    start by opening the XAMPP control panel
+    make sure that apache and mysql server is running
+    then click admin button in the mysql server
+    you will be directed to phpmyadmin page
+    then you need to create a database
+    */
+    //in the phpmyadmin you can go to user accounts
+    //to see users information
 ?>
