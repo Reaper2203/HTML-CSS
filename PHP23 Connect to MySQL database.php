e25ab21adee3c143f8572a80a2c258eaac998c0b
Reaper2203 <134066384+Reaper2203@users.noreply.github.com>
2024-02-12 08:42:21 +0800

    Update PHP23 Connect to MySQL database.php

--- PHP23 Connect to MySQL database.php
+++ PHP23 Connect to MySQL database.php
@@ -11,4 +11,17 @@
     */
     //in the phpmyadmin you can go to user accounts
     //to see users information
+
+    include("PHP23 database.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello
+</body>
+</html>
